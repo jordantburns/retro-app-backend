e3055d1182226c833998d01075032548b2ebcd40
Jordan <jordan.burns@stockport.gov.uk>
2020-02-04 16:48:57 +0000

    feat(Models): Add Room model to allow users to join rooms

---
+++ retro-app-backend/Controllers/RetroApp/Models/RetroRoom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace retro_app_backend.Controllers.RetroApp.Models
+{
+    public class RetroRoom
+    {
+        public string RoomCreator { get; set; }
+
+        public string Pin { get; set; }
+
+        public Guid id { get; set; } = Guid.NewGuid();
+    }
+}
--- retro-app-backend/Controllers/RetroApp/RetroAppController.cs
+++ retro-app-backend/Controllers/RetroApp/RetroAppController.cs
@@ -10,6 +10,8 @@ namespace retro_app_backend.Controllers.RetroApp
 {
     public class RetroAppController : Controller
     {
+        private static List<RetroRoom> _rooms = new List<RetroRoom>();
+
         [Route("{*url}")]
         [HttpGet]
         public IActionResult Index()
@@ -17,9 +19,11 @@ namespace retro_app_backend.Controllers.RetroApp
             return View("../Index");
         }
 
+        //TO DO: Room Controller for this where the base route is /room then cut and paste this here
+        //Then create a post with route "board" where the user can post to the board with a post it
         [Route("submit")]
         [HttpPost]
-        public async Task<IActionResult> Submit([FromBody] RetroAppDataModel model)
+        public async Task<IActionResult> CreateRoom([FromBody] RetroAppDataModel model)
         {
             if(string.IsNullOrEmpty(model.FirstName))
             {
@@ -29,6 +33,14 @@ namespace retro_app_backend.Controllers.RetroApp
             string pin = Guid.NewGuid().ToString();
 
             pin = pin.Substring(0, 6);
+
+            RetroRoom room = new RetroRoom
+            {
+                Pin = pin,
+                RoomCreator = model.FirstName
+            };
+
+            _rooms.Add(room);
 
             return Ok(pin);
         }
